be54dc8a63043f1714fd5b1f4226724afebdd483
Carolina Strecht <carolina.stsampaio@gmail.com>
2023-01-31 18:48:58 +0000

    WIP: Responsive Introduction section

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -10,43 +10,49 @@ const Intro = () => {
   return (
     <section id="intro">
       <div className="intro">
-        <div className="my-auto text-4xl">
-          <p className="pb-3">Hello, I'm Carolina Strecht ✌️</p>
+        <div className="my-auto text-2xl text-center sm:text-left md:text-4xl 2xl:text-5xl">
+          <p className="pb-3 2xl:pb-6">Hello, I'm Carolina Strecht ✌️</p>
           <div>
             <Typed strings={["I'm a front-end developer"]} typeSpeed={70} />
           </div>
-          <div className="socials-container">
-            <a
-              className="group"
-              href="https://github.com/cstrecht"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={github} alt="Github" className="social-icon" />
-              <div className="github-tooltip">Github</div>
-            </a>
-            <a
-              className="group"
-              href="https://www.linkedin.com/in/cstrecht/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={linkedin} alt="Linkedin" className="social-icon" />
-              <div className="linkedin-tooltip">LinkedIn</div>
-            </a>
-            <a
-              className="group"
-              href="https://twitter.com/cstrechtt"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={twitter} alt="Twitter" className="social-icon" />
-              <div className="twitter-tooltip">Twitter</div>
-            </a>
+          <div className="flex justify-center sm:justify-start">
+            <div className="socials-container">
+              <a
+                className="group"
+                href="https://github.com/cstrecht"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={github} alt="Github" className="social-icon" />
+                <div className="github-tooltip">Github</div>
+              </a>
+              <a
+                className="group"
+                href="https://www.linkedin.com/in/cstrecht/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={linkedin} alt="Linkedin" className="social-icon" />
+                <div className="linkedin-tooltip">LinkedIn</div>
+              </a>
+              <a
+                className="group"
+                href="https://twitter.com/cstrechtt"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={twitter} alt="Twitter" className="social-icon" />
+                <div className="twitter-tooltip">Twitter</div>
+              </a>
+            </div>
           </div>
         </div>
-        <div className="my-auto">
-          <img src={avatar1} className="h-96" alt="Carolina's logo" />
+        <div className="my-auto flex justify-center">
+          <img
+            src={avatar1}
+            className="h-72 mt-10 sm:mt-0 sm:h-80 md:h-96 2xl:h-[484px]"
+            alt="Carolina's logo"
+          />
         </div>
       </div>
     </section>

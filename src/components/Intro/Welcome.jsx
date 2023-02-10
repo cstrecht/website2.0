b3f9c3403a7208ad1df89766c6df4915b70311d6
Carolina Strecht <carolina.stsampaio@gmail.com>
2023-02-10 00:05:11 +0000

    change react-typed dep to updated one

--- src/components/Intro/Welcome.jsx
+++ src/components/Intro/Welcome.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import Typed from "react-typed";
+import Typed from "typed.js";
+import { useEffect, useRef } from "react";
 import Socials from "./Socials";
 
 const Welcome = () => {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["I'm a frontend developer."],
+      startDelay: 300,
+      typeSpeed: 50,
+    });
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div className="intro-text">
       <p className="intro-welcome">Hello, I'm Carolina Strecht ✌️</p>
-      <div>
-        <Typed strings={["I'm a front-end developer"]} typeSpeed={70} />
-      </div>
+      <span ref={el}></span>
       <Socials />
     </div>
   );

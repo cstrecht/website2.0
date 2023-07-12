3f2b72e040bf9f7743ccbc9f91a1aff8be36c9ae
Carolina Strecht <77847398+cstrecht@users.noreply.github.com>
2023-07-12 17:47:28 +0100

    Update CV to latest version

--- src/components/About/DownloadCV.jsx
+++ src/components/About/DownloadCV.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import CVCarolinaStrecht from "../../assets/CVCarolinaStrecht.pdf";
+import cv_carolina_strecht from "../../assets/cv_carolina_strecht.pdf";
 
 const DownloadCV = () => {
   return (
     <div className="download-btn-container">
       <button className="download-btn">
-        <a href={CVCarolinaStrecht} rel="noreferrer" target="_blank">
+        <a href={cv_carolina_strecht} rel="noreferrer" target="_blank">
           Download CV
         </a>
       </button>
